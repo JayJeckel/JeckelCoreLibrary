5624f34834d1dfda89200d01fbe55724f8f8a04b
JayJeckel <jeckelmail@gmail.com>
2015-03-27 14:08:06 -0500

    Closes #22

---
+++ java/jeckelcorelibrary/api/tiles/ITileTanker.java
@@ -0,0 +1,10 @@
+package jeckelcorelibrary.api.tiles;
+
+import java.util.List;
+
+import net.minecraftforge.fluids.FluidTank;
+
+public interface ITileTanker
+{
+	public List<FluidTank> getTanks();
+}
